b832511da33b8d5057fa0328f4fc750c1994c9f9
wiktoria-karczewska <wiktoria.karczewska@proton.me>
2023-08-07 12:55:36 +0200

    Zadanie 1

---
+++ main.js
@@ -0,0 +1,13 @@
+import "./style.css";
+
+document.querySelector("#app").innerHTML = `
+  <div>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`;
